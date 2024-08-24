a5a256d41ed88fb1983d22da4e5349f7b9883f9b
tanvir kamble <tanukamble22@gmail.com>
2024-08-24 20:01:29 +0530

    add home rout to svg

--- src/components/partials/NavigationBar.jsx
+++ src/components/partials/NavigationBar.jsx
@@ -1,16 +1,19 @@
 import { Navbar } from 'flowbite-react';
 import OffCanvasSidebar from './OffCanvasSidebar';
+import { Link } from 'react-router-dom';
 
 export function NavigationBar() {
   return (
     <Navbar className="border my-1">
       <Navbar.Brand>
         <div className="flex items-center space-x-2">
-          <img
-            src="/metamask-svgrepo-com.svg"
-            alt="MetaMask"
-            className="w-6 h-6"
-          />
+          <Link to="/">
+            <img
+              src="/metamask-svgrepo-com.svg"
+              alt="MetaMask"
+              className="w-6 h-6"
+            />
+          </Link>
           {/* Static SVG */}
           <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
             WalletInsight
